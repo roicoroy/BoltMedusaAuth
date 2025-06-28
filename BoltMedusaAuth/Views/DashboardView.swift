5eed99858f55ed48cc0c538a8859aad51b963748
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2025-06-28 14:43:01 +0100

    fixes

--- BoltMedusaAuth/Views/DashboardView.swift
+++ BoltMedusaAuth/Views/DashboardView.swift
@@ -274,24 +274,6 @@ struct AddressCard: View {
     }
 }
 
-struct DetailRow: View {
-    let title: String
-    let value: String
-    
-    var body: some View {
-        HStack {
-            Text(title)
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-            
-            Spacer()
-            
-            Text(value)
-                .font(.subheadline)
-                .fontWeight(.medium)
-        }
-    }
-}
 
 #Preview {
     let authService = AuthService()
